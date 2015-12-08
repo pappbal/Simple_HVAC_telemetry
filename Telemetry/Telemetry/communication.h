9f771179d8793eea56dcfd31cf4912fdfb04341a
Geza Husi <husigeza91@gmail.com>
2015-12-08 20:45:20 +0100

    Adding doxygen comments to communication.cpp.

--- Telemetry/Telemetry/communication.h
+++ Telemetry/Telemetry/communication.h
@@ -7,7 +7,10 @@
 #include <QIODevice>
 #include <QSerialPort>
 
-
+/**
+ * @brief The Communication class
+ * Base communication class, protocol specific communication classes are inherited from this
+ */
 class Communication : public QObject {
 
     Q_OBJECT
@@ -19,45 +22,43 @@ class Communication : public QObject {
         QByteArray data_stream_out;
 
     public:
-     Communication();
-     ~Communication();
+         Communication(){}
+         ~Communication(){}
 
      void sendData(Package package);
      Package getData() {return Package(received_data_stream);}
 
     signals:
         void signalToProxy();
-
-
 };
 
+/**
+ * @brief The Serial_Communication class
+ * Protocol specific class with specific functions
+ */
 class Serial_Communication : public Communication{
 
     Q_OBJECT
 
     QSerialPort Serial_port;
     //Class specific data sending
-    void sendData_specific();
+    void sendData_specific() override;
 
     bool start_arrived = false;
     bool header_arrived = false;
+
     int length_byte_arrive = 0;
     qint32 data_length = 0;
+
     QByteArray Temp_array_for_length;
     QByteArray Temp_array_for_data;
 
     public:
 
-        //Constructor
         Serial_Communication(qint32 baudRate,QSerialPort::DataBits dataBits,QSerialPort::Parity parity,QSerialPort::StopBits stopBits,QSerialPort::FlowControl flow_control);
-        //Destructor
         ~Serial_Communication();
 
-        //QSerialPort& getSerial_port() {return QSerialPort;}
         bool open_serial_port(QString portName);
-
-
-
 
 public slots:
         void read_received_data();
